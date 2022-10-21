d3d2b52825a874fd78b8b433f65f0fc59f034262
Rick Brown <ricky.brown.00@gmail.com>
2022-10-21 21:55:47 +0100

    cleans components

--- components/card.tsx
+++ components/card.tsx
@@ -13,7 +13,9 @@ const Card: NextPage<Props> = ({ imgUrl, title, subtitle }): JSX.Element => {
 			<div className='relative h-full'>
 				<Thumb imgUrl={imgUrl} />
 				<div className='absolute w-full bottom-0 px-4 py-2 rounded-b-xl bg-zinc-800'>
-					<h2 className='text-cyan text-center text-sm truncate'>{title}</h2>
+					<h2 className='text-cyan-400 text-center text-sm truncate'>
+						{title}
+					</h2>
 					{subtitle ? (
 						<p className='text-cyan-400 text-center text-xs truncate'>
 							{subtitle}
